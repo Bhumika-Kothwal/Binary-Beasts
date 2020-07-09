9881a7fd25fee7b68f5f4b40fc883cbc1514e643
Bhumika-Kothwal <64397046+Bhumika-Kothwal@users.noreply.github.com>
2020-07-09 22:07:59 +0530

    app uploaded

---
+++ app/basic/static/js/index.js
@@ -0,0 +1,72 @@
+const video = document.getElementById('video');
+const audio = document.getElementById('audio');
+
+var socket = io.connect('http://127.0.0.1:5000');
+socket.on( 'connect', function() {
+  console.log("SOCKET CONNECTED")
+})
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+Promise.all([
+  faceapi.loadFaceLandmarkModel("http://127.0.0.1:5000/static/models/"),
+  faceapi.loadFaceRecognitionModel("http://127.0.0.1:5000/static/models/"),
+  faceapi.loadTinyFaceDetectorModel("http://127.0.0.1:5000/static/models/"),
+  faceapi.loadFaceLandmarkModel("http://127.0.0.1:5000/static/models/"),
+  faceapi.loadFaceLandmarkTinyModel("http://127.0.0.1:5000/static/models/"),
+  faceapi.loadFaceRecognitionModel("http://127.0.0.1:5000/static/models/"),
+])
+  .then(startVideo)
+  .catch(err => console.error(err));
+
+function startVideo() {
+  var access = "access";
+  console.log(access);
+  navigator.getUserMedia(
+    {
+      video: {}
+    },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+video.addEventListener('play', () => {
+
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks();
+    socket.emit( 'my event', {
+      data: detections
+    })
+    socket.on('my response', function(msg){
+      console.log(msg)
+      console.log(typeof msg)
+      if(msg)
+      {
+        var str = "playing alarm";
+        console.log(str);
+        audio.play();
+        console.log(str);
+      }
+      else
+      {
+        audio.pause();
+        audio.currentTime=0;
+      }
+    })
+    
+
+
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+  }, 100)
+})
